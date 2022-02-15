8b250862f0d2468e395ee2a9069cfbf04016054a
Oliveira-Douglas <93629512+Oliveira-Douglas@users.noreply.github.com>
2022-02-15 03:36:17 -0300

    Delete Mais1Code directory

--- Mais1Code/Mais1Code/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mais1Code.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
